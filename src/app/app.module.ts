c656c9d46d2d034e18c4460866906fa4edde1943
danidi <danieldial2010@hotmail.com>
2017-10-03 22:54:08 -0500

    import de modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import{ClientesModule}from './clientes/clientes.module'
+import{ClientesModule}from './clientes/clientes.module';
+import{EmpleadosModule}from './empleados/empleados.module';
+import{ProductosModule}from './productos/productos.module';
+import{ProveedoresModule}from './proveedores/proveedores.module';
+import{OrdenesModule}from './ordenes/ordenes.module';
+import{DetalleModule}from './detalle/detalle.module';
+
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,7 +16,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    ClientesModule
+    ClientesModule,
+    EmpleadosModule,
+    ProductosModule,
+    ProveedoresModule,
+    OrdenesModule,
+    DetalleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
