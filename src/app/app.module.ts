5f6a66799a47e7da229e1943fbe4c41781e11af2
danidi <danieldial2010@hotmail.com>
2017-10-03 22:46:12 -0500

    import moduloclientes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{ClientesModule}from './clientes/clientes.module'
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ClientesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
